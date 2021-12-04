9d248c42f66baed7dc9fb275edc0289d50d1fb20
Julien Blasco <julien.blasco@mailbox.org>
2021-12-04 14:20:55 +0100

    Ajout textes

--- app.R
+++ app.R
@@ -9,6 +9,8 @@
 
 library(shiny)
 library(ggplot2)
+library(pdftools)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -19,23 +21,25 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("v",
-                        "Angle:",
-                        min = 0,
-                        max = 1,
-                        value = 0.5)
-        ),
-
+            fluidRow("Plutôt"),
+            fluidRow(textOutput('gauche')), 
+            fluidRow("ou"),
+            fluidRow(textOutput('droite'))
+            ),
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+        mainPanel(plotOutput("distPlot"))
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    cartes_tirees <- tirer_cartes(cartes)
 
+    output$text <- renderText(c("Plutôt", cartes_tirees$Gauche, "ou", cartes_tirees$Droite), sep="\n")
+    output$gauche <- renderText(cartes_tirees$Gauche)
+    output$droite <- renderText(cartes_tirees$Droite)
+    
     output$distPlot <- renderPlot({
         zones <- c(
             "01", "21", "31", "41", "32", "22",
@@ -47,19 +51,25 @@ server <- function(input, output) {
             largeur = c(10, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 1)
         )
         
-        v <- input$v
+        v <- tirer_valeur()
+        
         ggplot(roue, aes(x="", fill = zone, y = largeur)) + 
             geom_col() + 
             geom_polygon(
                 aes(x, y), inherit.aes = FALSE, fill="black",
-                data=data.frame(x = c(0.5, 1.5, 1.5, 0.5), y = c(tronque(7.5-30*v), tronque(7.5-30*v), tronque(22.5-30*v), tronque(22.5-30*v)))
+                data=data.frame(x = c(0.5, 1.5, 1.5, 0.5), y = c(tronque(7-30*v), tronque(7-30*v), tronque(23-30*v), tronque(23-30*v)))
             ) +
             geom_polygon(
                 aes(x, y), inherit.aes = FALSE, fill="black",
-                data=data.frame(x = c(0.5, 1.5, 1.5, 0.5), y = c(30-tronque(-7.5+30*v), 30-tronque(-7.5+30*v), 30-tronque(-22.5+30*v), 30-tronque(-22.5+30*v)))
+                data=data.frame(x = c(0.5, 1.5, 1.5, 0.5), y = c(30-tronque(-7+30*v), 30-tronque(-7+30*v), 30-tronque(-23+30*v), 30-tronque(-23+30*v)))
             ) +
             coord_polar(theta="y", direction=-1, start = v*2*pi) +
-            scale_fill_manual(values = c(rep(c("white", "yellow", "orange", "blue", "orange", "yellow"), 2)))    })
+            scale_fill_manual(values = c(rep(c("#f0f0f0", "yellow", "orange", "blue", "orange", "yellow"), 2))) +
+            geom_label(inherit.aes=FALSE, x=0, y=22.5-30*v, label=cartes_tirees$Gauche) +
+            geom_label(inherit.aes=FALSE, x=1, y=22.5-30*v, label=cartes_tirees$Droite) +
+            theme_void() + 
+            theme(legend.position = "none")
+        })
 }
 
 # Run the application 
--- fonctions.R
+++ fonctions.R
@@ -6,10 +6,19 @@ tirer_valeur <- function() {
   return(runif(1))
 }
 
-tirer_cartes <- function() {
-  return(list(
-    page = floor(runif(1, 1, 16)),
-    ligne = floor(runif(1, 1, 4)),
-    colonne = floor(runif(1, 1, 4))
-  ))
+tirer_cartes <- function(table) {
+  return(table[sample(1:nrow(table), 1), ])
 }
+
+cartes_tsv <- read.delim("./cartes.tsv", header = FALSE)
+
+cartes <- bind_rows(
+  transmute(cartes_tsv, Gauche = V1, Droite = V2),
+  transmute(cartes_tsv, Gauche = V3, Droite = V4),
+  transmute(cartes_tsv, Gauche = V5, Droite = V6),
+  transmute(cartes_tsv, Gauche = V7, Droite = V8),
+  ) %>% 
+  filter(Gauche != "") %>% 
+  mutate(across(c(Gauche, Droite), str_replace, "-[:space:]+", "")) %>% 
+  mutate(across(c(Gauche, Droite), str_replace, "^[:space:]", "")) %>% 
+  mutate(across(c(Gauche, Droite), str_replace, "[:space:][:space:]+", " "))
